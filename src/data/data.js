58cc13bc3955ae41374dcea6a68dbe6d286b3a06
Felipe Castro <felipdsousc@gmail.com>
2023-09-04 14:04:30 -0300

    finalizando o blog básico

---
+++ src/data/data.js
@@ -0,0 +1,1 @@
+export const links =  [{name: "home", href: "/"}, {name: "about", href: "/about"}, {name: "blog", href: "/blog"}]
---
+++ src/scripts/menu.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburguer').addEventListener('click', () => {
+    document.querySelector('.navbar').classList.toggle('expanded');
+    console.log("bananas")
+});
